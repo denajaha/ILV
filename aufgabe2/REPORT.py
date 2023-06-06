c66a42f75ac8896d6c1e6e4c504d5d5de4b9eff0
denajaha <jahic.dena@gmail.com>
2023-06-06 18:04:51 +0200

    report commit

---
+++ aufgabe2/REPORT.py
@@ -0,0 +1,52 @@
+'''
+Summary of Tasks and Findings:
+###
+Linear Interpolation between Slices
+###
+I have implemented a Python script to perform linear interpolation between two slices of a volume dataset.
+The volume dataset was loaded from a compressed numpy file format (npz).
+The script allowed for selecting consecutive slices and specifying the number of interpolated slices.
+Interpolation weights were computed to span the range [0, 1] and increase linearly.
+Linear interpolation was performed pixel-wise along the z-axis.
+The interpolated slices were computed using the formula: interpolated_value = (1 - weight) * value_1 + weight * value_2.
+
+###
+Interpolation Visualization
+###
+We modified the script to visualize the two selected slices to be interpolated and the interpolated slices.
+Each slice was displayed in individual figures that opened in full-screen size for easy comparison.
+The matplotlib library was used to create the figures and show the slices.
+
+###
+Interpolation with Non-Consecutive Slices
+###
+We extended the script to interpolate between non-consecutive slices that were further apart.
+The interpolated pixel values may not provide accurate estimates for the intermediate intensity values of the volume.
+Linear interpolation assumes a linear relationship between slices, which may not hold
+for non-consecutive slices that are distant from each other.
+Linear interpolation may not capture complex and non-linear variations in the data, leading to less accurate estimates.
+
+###
+Interpolation with Two Slices 2 Slices Apart
+###
+I have modified the script to interpolate between two slices that were 2 slices apart and compared the
+reconstructed slice with the ground truth slice.
+The interpolated slice was reconstructed by averaging the two neighboring slices.
+The mean squared error (MSE) between the interpolated slice and the ground truth slice was computed.
+The MSE quantified the difference between the interpolated slice and the ground truth slice, with a lower
+value indicating a better estimate.
+
+###
+Conclusion:
+###
+Linear interpolation can provide reasonable estimates when interpolating between consecutive slices in a volume dataset.
+ However, when dealing with non-consecutive slices or slices that are further apart, linear interpolation may not
+ accurately capture the underlying variations in the data. It is important to consider the nature of the data and
+ explore alternative interpolation methods for more accurate results. The comparison with ground truth slices helps to
+ assess the quality of the interpolation and identify any discrepancies.
+
+It is crucial to note that the effectiveness of interpolation techniques depends on the specific characteristics of
+the dataset and the requirements of the application.
+Domain-specific knowledge and more advanced interpolation methods may be required in certain cases.
+
+'''
